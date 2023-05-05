6525ae3affcfd89d839c6794886a2b516afe0354
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2023-05-05 21:35:41 +0100

    making spring creating beans and auto-wiring it instead of doing it manually

--- src/main/java/com/in28minutes/learnspringframework/GamingAppLauncherApplication.java
+++ src/main/java/com/in28minutes/learnspringframework/GamingAppLauncherApplication.java
@@ -2,11 +2,18 @@ package com.in28minutes.learnspringframework;
 
 import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.GamingConsole;
+import com.in28minutes.learnspringframework.game.PacmanGame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
+@ComponentScan("com.in28minutes.learnspringframework.game")
 public class App03GamingSpringBeans {
     public static void main(String[] args){
-        try(var context = new  AnnotationConfigApplicationContext(GamingConfiguration.class)) {
+        try(var context = new  AnnotationConfigApplicationContext(App03GamingSpringBeans.class)) {
             context.getBean(GamingConsole.class).up();
             context.getBean(GameRunner.class).run();
         }
